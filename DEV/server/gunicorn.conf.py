4b1f7ca174a86a3230f323dc55efeb265b280823
ricardoquinteladev <ricardo.dev185@gmail.com>
2023-10-29 15:26:13 +0000

    feat: changed gunicorn config

--- DEV/server/gunicorn.conf.py
+++ DEV/server/gunicorn.conf.py
@@ -1,5 +1,9 @@
+# gunicorn config file
 loglevel = "debug"
 errorlog = 'error_log.txt'
+accesslog = '-'
 
 workers = 1
 worker_class = "geventwebsocket.gunicorn.workers.GeventWebSocketWorker"
+
+access_log_format = "[%(t)s] %(m)s (%(s)s): '%(U)s%(q)s'"
