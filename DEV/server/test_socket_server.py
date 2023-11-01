56aa4883ae307c810281a69493c4d729b1579096
ricardo-quintela <ricardo.dev185@gmail.com>
2023-11-01 16:35:54 +0000

    test: added tests for leave event

--- DEV/server/test_socket_server.py
+++ DEV/server/test_socket_server.py
@@ -4,32 +4,46 @@
 import pytest
 from flask import Flask
 from flask.testing import FlaskClient
-from flask_socketio import SocketIO
 from flask_socketio.test_client import SocketIOTestClient
-from server import app, socket_server
+
+from server import app, socket_server, room_data
+from rooms import RoomData
+
+
+@pytest.fixture
+def rooms():
+    """Creates a new room
+
+    Drops it after not being used
+
+    Yields:
+        RoomData: a room data object with one room inside
+    """
+    room_data.create_room("AAAAA")
+    yield room_data
+    room_data.delete("AAAAA")
+
+#*================================================================
 
 @pytest.fixture
 def rest_api():
     app.config.update(
         {"TESTING": True}
     )
-    yield app
+    return app
+
+#*================================================================
+
 
 @pytest.fixture
 def client(rest_api: Flask) -> FlaskClient:
     return rest_api.test_client()
 
-
-
 @pytest.fixture
-def websocket() -> SocketIO:
-    yield socket_server
+def socket(rest_api: Flask) -> SocketIOTestClient:
+    return socket_server.test_client(rest_api)
 
-@pytest.fixture
-def socket(websocket: SocketIO, rest_api: Flask) -> SocketIOTestClient:
-    return websocket.test_client(rest_api)
-
-
+#*================================================================
 
 
 
@@ -39,16 +53,10 @@ def test_websocket_connected(socket: SocketIOTestClient):
     assert socket.is_connected() is True
 
 
-def test_websocket_event_join(socket: SocketIOTestClient, client: FlaskClient):
-    """Tests if the server emits a join event to everyone connected
+def test_websocket_event_join(socket: SocketIOTestClient, rooms: RoomData):
+    """Tests if the server emits a playerData event to
+    everyone connected after join event
     """
-
-    client.post(
-        "/testing/create-room",
-        data={
-            "roomId": "AAAAA"
-        }
-    )
 
     socket.emit("join", {
         "roomId": "AAAAA",
@@ -59,3 +67,21 @@ def test_websocket_event_join(socket: SocketIOTestClient, client: FlaskClient):
         "username": "testClient",
         "isReady": False
     }
+
+
+def test_websocket_event_leave(socket: SocketIOTestClient, rooms: RoomData):
+    """Tests if the server emits a playerData event to
+    everyone connected after leave room event
+    """
+
+    socket.emit("join", {
+        "roomId": "AAAAA",
+        "username": "testClient2"
+    })
+
+    socket.emit("leave", {
+        "roomId": "AAAAA"
+    })
+
+    assert rooms.get("AAAAA") == None
+
