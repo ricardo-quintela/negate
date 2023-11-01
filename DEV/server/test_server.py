70eff102d70a288577cbbe0c9cfbe6fe3d05c2c3
ricardo-quintela <ricardo.dev185@gmail.com>
2023-11-01 16:35:42 +0000

    refactor: added new fixtures and tests structure

--- DEV/server/test_server.py
+++ DEV/server/test_server.py
@@ -5,7 +5,15 @@ import pytest
 from flask import Flask
 from flask.testing import FlaskClient
 
-from server import app
+from server import app, room_data
+from rooms import RoomData
+
+@pytest.fixture
+def rooms():
+    room_data.create_room("AAAAA")
+    return room_data
+
+#*===========================================================
 
 @pytest.fixture
 def rest_api():
@@ -18,6 +26,7 @@ def rest_api():
 def client(rest_api: Flask) -> FlaskClient:
     return rest_api.test_client()
 
+#*===========================================================
 
 
 def test_home_route(client: FlaskClient):
@@ -26,6 +35,54 @@ def test_home_route(client: FlaskClient):
     response = client.get("/")
 
     assert response.status_code == 200
+
+
+def test_create_room_route(client: FlaskClient):
+    """Tests if create-room route can be accessed
+    """
+
+    response = client.get("/create-room?username=testClient")
+
+    assert response.status_code == 303
+
+
+def test_create_room_route_bad_req(client: FlaskClient):
+    """Tests if create-room route with bad query params is not accessed
+    """
+    response = client.get("/create-room")
+
+    assert response.status_code == 400
+
+
+def test_join_room_route(client: FlaskClient, rooms: RoomData):
+    """Tests if join-room route can be accessed
+    """
+    response = client.get("/join-room?roomId=AAAAA&username=testClient")
+
+    assert response.status_code == 303
+
+
+def test_join_room_route_bad_req(client: FlaskClient, rooms: RoomData):
+    """Tests if join-room route can be accessed
+    """
+    response = client.get("/join-room")
+
+    assert response.status_code == 400
+
+
+def test_game_route(client: FlaskClient, rooms: RoomData):
+    """Tests if an existing room can be accessed route can be accessed
+    """
+
+    response = client.get("/game/AAAAA?username=testClient")
+
+    assert response.status_code == 200
+
+
+
+
+
+#* ==========================================================
 
 
 def test_testing_create_room(client: FlaskClient):
