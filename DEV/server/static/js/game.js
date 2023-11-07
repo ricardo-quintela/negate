b3abfd190f9fe5f0c82c37a55a782378b7d226e4
ricardoquinteladev <ricardo.dev185@gmail.com>
2023-11-07 19:33:32 +0000

    feat: added leave event

--- DEV/server/static/js/game.js
+++ DEV/server/static/js/game.js
@@ -1,20 +1,34 @@
+// initializing player data object
+var playerData = {};
+var username = null;
+var roomId = null;
+var socket = null;
+
+
 document.addEventListener("DOMContentLoaded", () => {
 
-    const username = new URLSearchParams(window.location.search)
+    // getting roomId and username from request
+    username = new URLSearchParams(window.location.search)
                         .get("username");
+    roomId = window.location.pathname.slice(6);
 
-    const roomId = window.location.pathname.slice(6);
+    // connect to the socket
+    socket = io();
 
-    // getting socket
-    var socket = io();
-
+    // join the room
     socket.emit("join", { roomId: roomId, username: username });
     
 
     socket.on("playerData", (payload) => {
-
-        console.log(payload);
-
+        playerData = payload;
+        console.log(playerData);
     });
 
+
+});
+
+// disconnect from the room once the window closes
+window.addEventListener("beforeunload", (event) => {
+    event.preventDefault();
+    socket.emit("leave", { roomId: roomId });
 });
