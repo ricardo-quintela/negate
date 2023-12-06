745332a13868bb12d0358952ef8ab577fc3321e3
ricardoquinteladev <ricardo.dev185@gmail.com>
2023-12-06 11:42:42 +0000

    fix: renaming-items

---
+++ DEV/server/static/maps/spritesheet_interiors2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="spritesheet_interiors2" tilewidth="32" tileheight="32" tilecount="6016" columns="128">
+ <image source="../sprites/spritesheet_interiors2.png" trans="000000" width="4096" height="1504"/>
+</tileset>
